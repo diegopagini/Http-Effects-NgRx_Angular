89e3a8193f50e9ad04cf46dd6265008fee5a474d
Diego Pagini <dq@qbkconsulting.com>
2021-01-21 07:55:08 -0300

    maquetacion lista, ahora a implementar NgRx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, UsuariosModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    UsuariosModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/usuarios/lista/lista.component.ts
+++ src/app/usuarios/lista/lista.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
-  styleUrls: ['./lista.component.css']
+  styleUrls: ['./lista.component.css'],
 })
 export class ListaComponent implements OnInit {
-
-  constructor() { }
+  usuarios: Usuario[] = [];
+  constructor(public usuarioService: UsuarioService) {}
 
   ngOnInit(): void {
+    this.usuarioService.getUsers().subscribe((users) => {
+      this.usuarios = users;
+    });
   }
-
 }
